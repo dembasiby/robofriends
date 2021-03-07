3294b7272fb2be7845ffd92ce0a6fd5724e43968
Demba Siby <dembasiby@gmail.com>
2021-03-07 03:45:30 +0000

    Added deployment scripts

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import CardList from '../components/CardList'
+import ErrorBoundry from '../components/ErrorBoundry'
 import Scroll from '../components/Scroll'
 import SearchBox from '../components/SearchBox'
 import './app.css'
@@ -38,7 +39,9 @@ class App extends Component {
             <h1 className="f1">RoboFriends</h1>
             <SearchBox searchChange={this.onSearchChange}/> 
             <Scroll>
+                <ErrorBoundry>
                 <CardList robots={filteredRobots}/>
+                </ErrorBoundry>
             </Scroll>
         </div>
         )
